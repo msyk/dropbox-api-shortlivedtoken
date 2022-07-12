52424ef8e93b6986771c347d73b4e2f8ff1e9fcb
msyk <nii@msyk.net>
2022-07-12 12:07:27 +0900

    Ver.5 Redefine the download and downloadZip methods.

--- src/DropboxClientModified.php
+++ src/DropboxClientModified.php
@@ -25,4 +25,52 @@ class DropboxClientModified extends Client
         }
         return json_decode($response->getBody(), true) ?? [];
     }
+
+    /*
+     * download and downloadZip methods weren't work fine because StreamWrapper::getResource() returns null
+     * in spite of the $response->getBody() respond something. So the return process of both method changed
+     * as like. These returns the contents of file itself. 2022-7-12 msyk (nii@msyk.net)
+     */
+    /**
+     * Download a file from a user's Dropbox.
+     *
+     * @param string $path
+     *
+     * @return string
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-download
+     */
+    public function download(string $path)
+    {
+        $arguments = [
+            'path' => $this->normalizePath($path),
+        ];
+
+        $response = $this->contentEndpointRequest('files/download', $arguments);
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * Download a folder from the user's Dropbox, as a zip file.
+     * The folder must be less than 20 GB in size and have fewer than 10,000 total files.
+     * The input cannot be a single file. Any single file must be less than 4GB in size.
+     *
+     * @param string $path
+     *
+     * @return string
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-download_zip
+     */
+    public function downloadZip(string $path)
+    {
+        $arguments = [
+            'path' => $this->normalizePath($path),
+        ];
+
+        $response = $this->contentEndpointRequest('files/download_zip', $arguments);
+
+        return $response->getBody()->getContents();
+    }
+
 }
